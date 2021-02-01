7acce215339c7f691aa75388f8d576f7106712f1
Gerardo <you@example.com>
2021-01-31 22:24:59 -0600

    Helper Functions Update. Country FIles created

---
+++ custom_functions.py
@@ -0,0 +1,120 @@
+from googleapiclient.discovery import build
+import pandas as pd
+from datetime import datetime,date
+import time
+import os.path
+from os import path
+
+c = ["AR","BR","CA","CL","CO","ES","MX","US"]
+
+## API CALL + MERGE WITH CATEGORY ID + language
+def get_videos(country,title_v,date_new,hour):
+    api_key= input("API_KEY: ")
+    youtube = build("youtube","v3", developerKey=api_key)
+
+    categories = []
+    top_videos = []
+
+    chart_mx= youtube.videos().list(
+    part=["id","snippet","statistics"],
+    chart="mostPopular",
+    regionCode=country,
+    maxResults=50).execute()
+    
+    category_id= youtube.videoCategories().list(
+    part=["snippet"],
+    regionCode=country).execute()
+
+    for i in category_id["items"]:
+        categories.append({
+            "category_id": i["id"],
+            "category_title": i["snippet"]["title"]
+        })
+    
+    for i in chart_mx["items"]:
+        vid_id=i["id"]
+        ytb_link=f"https://youtu.be/{vid_id}"
+        tags = None
+        language = None
+
+        try:
+            i["snippet"]["defaultAudioLanguage"]
+        except KeyError:
+            language = ""
+        
+        if language != "":
+            language = i["snippet"]["defaultAudioLanguage"]
+            
+        try:
+            i["snippet"]["tags"]
+        except KeyError:
+            tags = ""
+
+        if tags!="":
+            tags = i["snippet"]["tags"]
+    
+        top_videos.append({
+            "published_date":i["snippet"]["publishedAt"],
+            "trending_date":f"{date_new}T{hour}:00:00Z",
+            "category_id":i["snippet"]["categoryId"],
+            "channel_title":i["snippet"]["channelTitle"],
+            "tags":tags,
+            "video_title":i["snippet"]["title"],
+            "views":i["statistics"]["viewCount"], 
+            "likes":i["statistics"]["likeCount"],
+            "dislikes":i["statistics"]["dislikeCount"],
+            "comments":i["statistics"].get("commentCount"),
+            "description":i["snippet"]["description"],
+            "channel_id":i["snippet"]["channelId"],
+            "link":ytb_link,
+            "thumbnail":i["snippet"]["thumbnails"]["medium"]["url"],
+            "hour_trending":hour,
+            "video_lang":language,
+            "count": 1,
+            "country":country
+        })
+
+    categories_df = pd.DataFrame.from_dict(categories)
+    top_videos_df = pd.DataFrame.from_dict(top_videos)
+    df = pd.merge(top_videos_df, categories_df,on ='category_id', how ='inner').to_csv(f"trending_videos_data/{country}/0{datetime.now().month}/{title_v}/test_file.csv")
+    print("test_file.csv created!")
+
+
+
+def merge_day(country,title_v,title,hour):
+    frames=[]
+
+    for i in range(hour):
+        if path.exists(f"trending_videos_data/{country}/0{datetime.now().month}/{title_v}/{title[0:11]}{i}.csv"): 
+            frames.append(pd.read_csv(f"trending_videos_data/{country}/0{datetime.now().month}/{title_v}/{title[0:11]}{i}.csv", index_col=0))
+
+    main_df = pd.concat(frames).drop_duplicates(subset=['video_title'], keep='last').to_csv(f"trending_videos_data/{country}/0{datetime.now().month}/{title_v}/merged_file.csv")
+    print("merged_file.csv created!")
+
+
+def union(country,hour,title_v,title):
+    frames=[]
+
+    for i in range(hour):
+        if path.exists(f"trending_videos_data/{country}/0{datetime.now().month}/{title_v}/{title[0:11]}{i}.csv"): 
+            frames.append(pd.read_csv(f"trending_videos_data/{country}/0{datetime.now().month}/{title_v}/{title[0:11]}{i}.csv", index_col=0))
+
+    main_df = pd.concat(frames).to_csv(f"trending_videos_data/{country}/0{datetime.now().month}/{title_v}/union_file.csv")
+    print("union_file.csv created!")
+
+    
+def todays_folder(title_v):
+    parent_dir = "D:/youtube/youtube_research/trending_videos_data/"
+    path = os.path.join(parent_dir, title_v) 
+    os.makedirs(path) 
+
+    print("Directory '% s' created" % title_v) 
+       
+
+if __name__ == "__main__":
+    get_videos()
+    merge_day()
+    union()
+    todays_folder()
+
+
